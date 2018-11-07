305d29d02231b04040a7f8cdaba847dc5ca3fbae
Mathias Bojda <mathiasbojda@gmail.com>
2018-11-07 13:34:15 +0100

    Binary Gap

---
+++ BinaryGap.js
@@ -0,0 +1,25 @@
+
+
+function solution(N) {
+
+  if ( N < 1 || N > 2147483647) {
+    return 0;
+  }
+
+  const s = N.toString(2);
+  const re = /1(0*)(?=1)/g;
+  const matches = s.match(re);
+
+  let biggest = 0;
+
+  if ( !matches || !matches.length)
+    return 0;
+
+  for ( var i = 0; i < matches.length; i++ ){
+    if ( (matches[i].length-1) > biggest)
+      biggest = matches[i].length-1;
+  }
+
+  return biggest;
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CodilityAlgorithms
+
+Algorithmic Training from codility.com.
+These are my solutions 
+
+All the solutions i have solved myself.
+I only commit solutions that have 100%
